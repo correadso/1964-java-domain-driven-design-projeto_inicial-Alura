72117235edf25fbaaca05aa20c69e950f34662ee
Diogo <diogocorrea@tecnomotor.com.br>
2022-07-09 00:33:46 -0300

    Separando os contextos
    Criando os pacotes academico e gamificacao para separar os contextos da aplicação. Está separando por pacotes, mas poderiam ser módulos do Java 9 ou microserviços ou projetos separados. Dentro de cada pacote tem os pacotes aplicacao, domínio e infraestrutura.

---
+++ src/test/java/br/com/alura/escola/academico/dominio/aluno/AlunoTest.java
@@ -0,0 +1,48 @@
+package br.com.alura.escola.academico.dominio.aluno;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.academico.dominio.aluno.Aluno;
+import br.com.alura.escola.academico.dominio.aluno.CPF;
+import br.com.alura.escola.academico.dominio.aluno.Email;
+
+class AlunoTest {
+
+	private Aluno aluno;
+	private String ddd = "11";
+	private String numero = "123456789";
+	
+	@BeforeEach
+	void beforeEach() {
+		this.aluno = new Aluno(
+				new CPF("123.123.123-90"),
+				"fulano de tal",
+				new Email("fulano@fulano.com"));
+	}
+	
+	@Test
+	void deveriaPermitirAdicionar1Telefone() {
+		this.aluno.adicionarTelefone(ddd, numero);
+		assertEquals(1, this.aluno.getTelefones().size());
+	}
+
+	@Test
+	void deveriaPermitirAdicionar2Telefone() {
+		this.aluno.adicionarTelefone(ddd, numero);
+		this.aluno.adicionarTelefone(ddd, numero);
+		assertEquals(2, this.aluno.getTelefones().size());
+	}
+	
+	@Test
+	void naoDeveriaPermitirAdicionar3Telefones() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			this.aluno.adicionarTelefone(ddd, numero);
+			this.aluno.adicionarTelefone(ddd, numero);
+			this.aluno.adicionarTelefone(ddd, numero);
+		});
+	}
+	
+}
